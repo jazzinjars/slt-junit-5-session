edb92c25f2c4af21bedcb22bc2b0917929a740e3
Héctor Ardila López <hectorardila@gmail.com>
2018-12-11 16:15:11 +0100

    Initial commit for the junit5 session

---
+++ src/main/java/com/jazzinjars/junit5session/Junit5SessionApplication.java
@@ -0,0 +1,12 @@
+package com.jazzinjars.junit5session;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Junit5SessionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Junit5SessionApplication.class, args);
+	}
+}
